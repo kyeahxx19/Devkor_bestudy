a56c81c1ccb1e0832c325ba73b5d1be1841f0c26
gohome <39901387+dmobtxx19@users.noreply.github.com>
2023-09-21 23:38:12 +0900

    feat: user post/delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
@@ -17,6 +17,26 @@ export class AppController {
     try{
       const username = this.appService.getUsernameByIndex(index);
       res.send(username);
+    } catch(error){
+      res.status(400).send(error.message);
+    }
+  }
+
+  @Post('/:name')
+  postUsername(@Param('name') name: string, @Res() res: Response){
+    try{
+      const index = this.appService.postUsername(name);
+      res.status(200).send(index);
+    } catch(error){
+      res.status(500).send(error.message);
+    }
+  }
+
+  @Delete("/:index")
+  deleteUserByIndex(@Param('index') index: number, @Res() res: Response){
+    try{
+      const username = this.appService.deleteUserByIndex(index);
+      res.status(200).send(username);
     } catch(error){
       res.status(400).send(error.message);
     }
